c772631bc38d079ad91e8ded047c29da07522f1c
rathc02 <rathc02@uwosh.edu>
2022-09-02 03:58:29 -0500

    First animations!

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import { React } from 'react';
 import { makeStyles } from "@mui/styles"
 import { Box, Typography, Button, ImageList, ImageListItem, Unstable_Grid2 as Grid} from '@mui/material';
-import WCDrums from '../Images/WCD.png';
-import Audrie from '../Images/AudrieM.png'
-import Cru22 from '../Images/Cru22M.png';
 import { ArrowDropDown } from '@mui/icons-material';
+import { useSpring, animated } from 'react-spring';
 
 const useStyles = makeStyles((theme) => ({
     contain: { 
@@ -23,42 +21,59 @@ const useStyles = makeStyles((theme) => ({
 
   const classes = useStyles();
 
+  const animate1 = useSpring( 
+    {
+      from: {y: -100},
+      to: {y: 0}
+    });
+
+  const animate2 = useSpring( 
+    {
+      from: {y: 20000},
+      to: {y: 15}
+    });
+
+  const AnimatedText = animated(Typography);
   const itemData = [
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/WCD.png?raw=true',
-      title: 'Breakfast',
+      title: 'drums',
     },
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/SM.png?raw=true',
-      title: 'Burger',
-    },
-    {
-      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/AudrieM.png?raw=true',
-      title: 'Camera',
-    },
-    {
-      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/Cru22M.png?raw=true',
-      title: 'Coffee',
+      title: 'sm',
     },
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/Tristan.png?raw=true',
-      title: 'Hats',
+      title: 'tristan',
     },
     {
-      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/vacuum.png?raw=true',
-      title: 'Honey',
+      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/Cru22M.png?raw=true',
+      title: 'cru22',
+    },
+    {
+      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/AudrieM.png?raw=true',
+      title: 'audrie',
+    },
+    {
+      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/vacuum.png?raw=trMue',
+      title: 'vacuum',
     },
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/dad.png?raw=true',
-      title: 'Basketball',
+      title: 'dad',
     },
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/STNW_.png?raw=true',
-      title: 'Fern',
+      title: 'stnw',
     },
     {
       img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/HomeDrums.png?raw=true',
-      title: 'Mushrooms',
+      title: 'drums2',
+    },
+    {
+      img: 'https://github.com/rathclayton1/personal-website/blob/master/src/Images/backpack.png?raw=true',
+      title: 'backpack',
     },
   ];
 
@@ -67,22 +82,22 @@ const useStyles = makeStyles((theme) => ({
     <Grid container className={classes.contain} spacing={1} direction="column" overflow="auto">
       <Grid item>
         <Grid container spacing={3} justify="flex-end" alignItems="normal" direction="row">
-          <Grid item marginTop="7em" xs={8} sm={6}>
-            <Typography variant="h1" align="left" fontFamily="Silkscreen">
+          <Grid item marginTop="7em" xs={12} md={6}>
+            <AnimatedText variant="h1" align="left" fontFamily="Silkscreen" style={animate1}>
               Hello!
-            </Typography>
-            <Typography variant="h5" marginLeft="0.4em" marginTop="3em" fontFamily="WorkSans" align="left">
+            </AnimatedText>
+            <AnimatedText variant="h5" marginLeft="0.4em" marginTop="3em" fontFamily="WorkSans" align="left" style={animate2}>
               My name is Clayton Rath. I am a native Wisconsinite with a passion for blah, blah, and blah. 
               I decided to create this full-stack website from the ground up to encapsulate those three interests (and maybe more), as well as to enhance my software development skills.
-            </Typography>
+            </AnimatedText>
             <Button size="large" style={{fontFamily: "Silkscreen", fontSize: "1.2em", color: "black", marginLeft: "0.4em", marginTop: "1.5em"}} variant="contained">
               Check it out!
               <ArrowDropDown/>
             </Button>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} md={6}>
             <Grid container direction="row" spacing={0} xs={12} overflow="auto" disableEqualOverflow>
-              <ImageList variant="masonry" cols={3} gap={8}>
+              <ImageList variant="masonry" cols={3} gap={12}>
                 {itemData.map((item) => (
                   <ImageListItem key={item.img}>
                     <img
